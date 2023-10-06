92ac918a0780053d6a83fa9666b6ffe7955fef73
Yogendra Manawat <yogendramanawat@gmail.com>
2023-10-06 16:34:37 +0530

    display changed to standalone

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1696589563|15396986944";
+const CACHE_VERSION = "1696590242|16075930063";
 const CACHE_PREFIX = "Coursefy-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
